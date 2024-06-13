36469fb146e356c3796f681b27af7ced4f6dc0bd
Michaelanbinh <hoangkhoaan8@gmail.com>
2024-06-14 00:13:16 +0700

    4

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 onload = () =>{
     document.body.classList.remove("container");
 };
+
+document.addEventListener('DOMContentLoaded', () => {
+    const messageOverlay = document.getElementById('messageOverlay');
+    const cancelButton = document.getElementById('cancelButton');
+
+    // Show the message after 10 seconds
+    setTimeout(() => {
+        messageOverlay.style.display = 'flex';
+    }, 6000);
+
+    // Hide the message and return to the flower page when cancel button is clicked
+    cancelButton.addEventListener('click', () => {
+        messageOverlay.style.display = 'none';
+    });
+});
